5a03b708d630e726dc3b4be903dcb9e12394849c
zhangzhiguo-fighting <2205502072qq.com>
2020-08-08 18:04:07 +0800

    2020-08-08__18:03:59

--- 31.template.cpp
+++ 31.template.cpp
@@ -82,15 +82,22 @@ void printAny(const T &a, ARGS... args) {
     return ;
 }
 
+
+
 //帮助模板类展开的模板类
-template<typename T, typename ...ARGS>
+template<int n, typename T, typename ...ARGS> //为什么传入 int n编译期常量? 因为模板在编译后就不存在了
 struct ARG {
-    typedef T __type;
-    typedef ARG<ARGS...> __rest;
+    typedef typename ARG<n - 1, ARGS...>::__type __type;
+    //typedef ARG<n - 1, ARGS...> __rest;
+};
+
+template<typename T, typename ...ARGS>
+struct ARG<0, T, ARGS...> {
+   typedef T __type; 
 };
 
 template<typename T>
-struct ARG<T> {
+struct ARG<0, T> {
     typedef T __type;
 };
 
@@ -99,17 +106,94 @@ template<typename T, typename ...ARGS> class Test; //工具模板类，去解析
 template<typename T, typename ...ARGS>
 class Test<T(ARGS...)> { //使用偏特化形式，让这个模板类使用更像一个函数
 public:
-    T operator()(
-        typename ARG<ARGS...>::__type a, 
-        typename ARG<ARGS...>::__rest::__type b) {
-        return a + b;
+    T operator()(ARGS... args) { //函数的入口写起
+        return add<T>(args...);   
     }
+        //typename ARG<0, ARGS...>::__type a, //前面的typename主要是用来说明 后的的内容是一个类型（模板中的内置类型），避免语义歧义。
+private:
+    template<typename T1, typename U, typename ...US>
+        T1 add(U a, US ...args) {
+            return a + add<T1>(args...);
+        }
+    template<typename T1, typename U>
+        T1 add(U a) {
+            return a;
+        }
 };
 
-
+//模板函数作为参数，传递给函数的间接类型推导
+template<typename T, typename U>
+T test_param_func(U a) {
+    return a * 2;
 }
 
+void func2(int (*func)(double)) {
+    cout << func(2.3) << endl;
+}
+
+
+//remove——reference
+template<typename T> struct remove_reference { typedef T type; }; //正常值版本
+
+template<typename T> struct remove_reference<T &> { //左值引用偏特化版本
+    typedef T type;
+};
+
+template<typename T> struct remove_reference<T &&> { //右值引用偏特化版本
+    typedef T type;
+};
+
+//模板中的引用折叠
+template<typename T>
+typename remove_reference<T>::type add2(T &&a, T&&b) {
+    typename remove_reference<T>::type c = a + b; //不使用remove会造成把右值赋值给左值引用
+    return c;
+}
+
+//add_const
+template<typename T> struct add_const { typedef const T type; };
+template<typename T> struct add_const<const T> { typedef const T type; }; //T匹配到int
+
+
+//add_lvalue_reference
+template<typename T> struct add_lvalue_reference { typedef T& type; };
+template<typename T> struct add_lvalue_reference <T &>{ typedef T& type; };
+template<typename T> struct add_lvalue_reference<T &&> { typedef T& type; };
+
+//add_rvalue_reference
+template<typename T> struct add_rvalue_reference { typedef T&& type; };
+template<typename T> struct add_rvalue_reference<T &> { typedef T&& type; };
+template<typename T> struct add_rvalue_reference<T &&> { typedef T&& type; };
+
+//remove_pointer
+template<typename T> struct remove_pointer { typedef T type; };
+template<typename T> struct remove_pointer<T *> { typedef typename remove_pointer<T>::type type; };
+
+
+//move
+template<typename T>
+typename add_rvalue_reference<T>::type move(T &&a) { //&& 接收引用，左值变量会被推导为左值引用类型，右值会被推导为普通类型
+    return typename add_rvalue_reference<T>::type(a); //C++ 中的强转 类型(变量)
+}
+
+void f(int &x) {
+    cout << "f function : left value" << endl;
+    return ;
+}
+void f(int &&x) {
+    cout << "f function : right value" << endl;
+    return ;
+}
+
+
+
+} // end of haizei
+
 int main() {
+    int inta = 123, intb = 456;
+    cout << haizei::add2(inta, intb) << endl;
+    cout << haizei::add2(123, 456) << endl;
+    haizei::func2(haizei::test_param_func); //
     haizei::A a(1000);
     haizei::B b(645);
     cout << a + b << endl;
@@ -140,6 +224,14 @@ int main() {
     haizei::printAny(123, 34.5, "hello world", &a);
     haizei::Test<int(int, int)> f3; //函数对象
     cout << f3(3, 4) << endl;
+    
+    haizei::Test<int(int, int, int, int)> f4;
+    cout << f4(1, 2, 3, 4) << endl;
+    haizei::Test<int(int, int, int, int, int)> f5;
+    cout << f5(1, 2, 3, 4, 5) << endl;
+    
+    haizei::f(n);
+    haizei::f(haizei::move(m));
     return 0;
 }
 
